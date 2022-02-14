148ad29e3851ed56536bdd8c14c862db14cc64ff
Jude Adham <54722521+jadham2@users.noreply.github.com>
2022-02-14 16:35:17 -0500

    Ja/databases (#8)
    * Created models for database tables.
    
    * Fixed folder layout, added app to INSTALLED_APPS, and configured postgres in settings
    
    * gitignore and requirements changes
    
    * Fixed workflows.
    
    * only use flake8 as our Python linter
    
    * flake8 linting fixes.
    
    Co-authored-by: jess-jpg-txt <jesszhang24@gmail.com>

---
+++ backend/MakeMyDegree/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+    degree = models.ForeignKey(
+        'Degree',
+        on_delete=models.CASCADE,
+    )
+    curr_plan = models.JSONField()
+
+
+class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=100)
+    course_tag = models.CharField(max_length=15)
+    course_credits = models.IntegerField()
+    desciption = models.TextField()
+    terms = ArrayField(
+        models.CharField(max_length=6)
+    )
+
+
+class Requisite(models.Model):
+    course_id = models.ForeignKey(
+        'Course',
+        on_delete=models.CASCADE,
+        related_name="courses"
+    )
+    requisite = models.ForeignKey(
+        'Course',
+        on_delete=models.CASCADE,
+        related_name="requisites"
+    )
+    requisite_type = models.CharField(max_length=50)
+
+
+class Degree(models.Model):
+    degree_id = models.AutoField(primary_key=True)
+    degree_type = models.CharField(max_length=50)
+    degree_name = models.CharField(max_length=50)
+    school = models.CharField(max_length=100)
+    term = models.CharField(max_length=50)
+
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    degree_id = models.ForeignKey(
+        'Degree',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50)
+    rule = models.CharField(max_length=50)
+
+
+class CourseTag():
+    tag_id = models.ForeignKey(
+        'Tag',
+        on_delete=models.CASCADE,
+    )
+    course_id = models.ForeignKey(
+        'Course',
+        on_delete=models.CASCADE,
+    )
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MakeMyDegree',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 

c49c3b0785b1be08083ca015aa38c5c4cea7ff2b
Jude Adham <judeadham@gmail.com>
2022-02-20 21:10:54 -0500

    refactored tests.

--- backend/MakeMyDegree/tests.py
+++ backend/MakeMyDegree/tests.py
@@ -5,12 +5,123 @@ from MakeMyDegree.models import *
 
 
 class UserDegreeTests(APITestCase):
-    def test_create_degree_valid(self):
-        test_degree_data = {
+    def setUp(self):
+        compe_degree_data = {
             'degree_type': 'BS',
             'degree_name': 'Computer Engineering',
             'school': 'ECE',
             'term': 'Fa2019'
+        }
+        self.compe_degree = Degree.objects.create(**compe_degree_data)
+        self.compe_degree.save()
+
+        test_student_data = {
+            'name': 'Jude Lei',
+            'password': 'HelloWorld!@#',
+            'email': 'jadham@purdue.edu',
+            'degree': self.compe_degree,
+            'curr_plan': {}
+        }
+        self.test_student = User.objects.create(**test_student_data)
+        self.test_student.save()
+
+    def test_create_user_valid(self):
+        test_user_data = {
+            'name': 'Jess Zhang',
+            'password': 'graphsRox',
+            'email': 'jzhang@purdue.edu',
+            'degree': self.compe_degree.degree_id,
+            'curr_plan': {}
+        }
+        user_resp = self.client.post(
+            reverse('create_get_users'),
+            data=test_user_data,
+            format='json'
+        )
+
+        self.assertEqual(user_resp.status_code, status.HTTP_201_CREATED)
+        user_resp = user_resp.json()
+        self.assertTrue('user_id' in user_resp)
+
+    def test_create_user_invalid(self):
+        test_user_data = {
+            'name': 'Jess Zhang',
+            'password': 'graphsRox',
+            'email': 'jzhang@purdue.edu',
+            'curr_plan': {}
+        }
+        user_resp = self.client.post(
+            reverse('create_get_users'),
+            data=test_user_data,
+            format='json'
+        )
+
+        self.assertEqual(user_resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_all_users_update(self):
+        update_test_user_data = {
+            'name': 'Jezz Zhang',
+            'password': 'b293jfsq32',
+            'email': 'jzhang@purdue.edu',
+            'degree': self.compe_degree,
+            'curr_plan': {}
+        }
+        test_user = User.objects.create(**update_test_user_data)
+        test_user.save()
+
+        get_users_resp = self.client.get(
+            reverse('create_get_users'),
+            format='json'
+        )
+        get_users_resp = get_users_resp.json()
+
+        self.assertEqual(len(get_users_resp), 2)
+
+    def test_get_user_id_valid(self):
+        get_user_resp = self.client.get(
+            reverse('detail_user', args=[self.test_student.user_id]),
+            format='json'
+        )
+        get_user_resp = get_user_resp.json()
+
+        self.assertEqual(self.test_student.user_id, get_user_resp['user_id'])
+
+    def test_get_user_id_invalid(self):
+        get_user_resp = self.client.get(
+            reverse('detail_user', args=[1]),
+            format='json'
+        )
+
+        self.assertEqual(get_user_resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class DegreeTests(APITestCase):
+    def setUp(self):
+        compe_degree_data = {
+            'degree_type': 'BS',
+            'degree_name': 'Computer Engineering',
+            'school': 'ECE',
+            'term': 'Fa2019'
+        }
+        self.compe_degree = Degree.objects.create(**compe_degree_data)
+        self.compe_degree.save()
+
+        test_student_data = {
+            'name': 'Jude Lei',
+            'password': 'HelloWorld!@#',
+            'email': 'jadham@purdue.edu',
+            'degree': self.compe_degree,
+            'curr_plan': {}
+        }
+        self.test_student = User.objects.create(**test_student_data)
+        self.test_student.save()
+
+    def test_create_degree_valid(self):
+        test_degree_data = {
+            'degree_type': 'BA',
+            'degree_name': 'English',
+            'school': 'ENGL',
+            'term': 'Fa2020'
         }
         degree_resp = self.client.post(
             reverse('create_get_degrees'),
@@ -35,151 +146,3 @@ class UserDegreeTests(APITestCase):
         )
 
         self.assertEqual(degree_resp.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_create_user_valid(self):
-        test_degree_data = {
-            'degree_type': 'BS',
-            'degree_name': 'Computer Engineering',
-            'school': 'ECE',
-            'term': 'Fa2019'
-        }
-        degree_resp = self.client.post(
-            reverse('create_get_degrees'),
-            data=test_degree_data,
-            format='json'
-        )
-
-        self.assertEqual(degree_resp.status_code, status.HTTP_201_CREATED)
-        degree_resp = degree_resp.json()
-        self.assertTrue('degree_id' in degree_resp)
-
-        test_user_data = {
-            'name': 'Jude Lei',
-            'password': 'graphsRox',
-            'email': 'jadham@purdue.edu',
-            'degree': degree_resp['degree_id'],
-            'curr_plan': {}
-        }
-        user_resp = self.client.post(
-            reverse('create_get_users'),
-            data=test_user_data,
-            format='json'
-        )
-
-        self.assertEqual(user_resp.status_code, status.HTTP_201_CREATED)
-        user_resp = user_resp.json()
-        self.assertTrue('user_id' in user_resp)
-
-    def test_create_user_invalid(self):
-        test_degree_data = {
-            'degree_type': 'BS',
-            'degree_name': 'Computer Engineering',
-            'school': 'ECE',
-            'term': 'Fa2019'
-        }
-        degree_resp = self.client.post(
-            reverse('create_get_degrees'),
-            data=test_degree_data,
-            format='json'
-        )
-
-        self.assertEqual(degree_resp.status_code, status.HTTP_201_CREATED)
-        degree_resp = degree_resp.json()
-        self.assertTrue('degree_id' in degree_resp)
-
-        test_user_data = {
-            'name': 'Jude Lei',
-            'password': 'graphsRox',
-            'email': 'jadham@purdue.edu',
-            'curr_plan': {}
-        }
-        user_resp = self.client.post(
-            reverse('create_get_users'),
-            data=test_user_data,
-            format='json'
-        )
-
-        self.assertEqual(user_resp.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_get_all_users_empty(self):
-        get_users_resp = self.client.get(
-            reverse('create_get_users'),
-            format='json'
-        )
-        get_users_resp = get_users_resp.json()
-
-        self.assertFalse(get_users_resp)
-
-    def test_get_all_users_nonempty(self):
-        test_degree_data = {
-            'degree_type': 'BS',
-            'degree_name': 'Computer Engineering',
-            'school': 'ECE',
-            'term': 'Fa2019'
-        }
-        test_degree = Degree.objects.create(**test_degree_data)
-        test_degree.save()
-
-        test_user_data = {
-            'name': 'Jezz Zhang',
-            'password': 'b293jfsq32',
-            'email': 'jzhang@purdue.edu',
-            'degree': test_degree,
-            'curr_plan': {}
-        }
-        test_user = User.objects.create(**test_user_data)
-        test_user.save()
-
-        test_user_data = {
-            'name': 'Jude Adham',
-            'password': 'gjioe4h4eiog',
-            'email': 'jadham@purdue.edu',
-            'degree': test_degree,
-            'curr_plan': {}
-        }
-        test_user = User.objects.create(**test_user_data)
-        test_user.save()
-
-        get_users_resp = self.client.get(
-            reverse('create_get_users'),
-            format='json'
-        )
-        get_users_resp = get_users_resp.json()
-
-        self.assertEqual(len(get_users_resp), 2)
-
-    def test_get_user_id_valid(self):
-        test_degree_data = {
-            'degree_type': 'BS',
-            'degree_name': 'Computer Engineering',
-            'school': 'ECE',
-            'term': 'Fa2019'
-        }
-        test_degree = Degree.objects.create(**test_degree_data)
-        test_degree.save()
-
-        test_user_data = {
-            'name': 'Jezz Zhang',
-            'password': 'b293jfsq32',
-            'email': 'jzhang@purdue.edu',
-            'degree': test_degree,
-            'curr_plan': {}
-        }
-        test_user = User.objects.create(**test_user_data)
-        test_user.save()
-
-        get_user_resp = self.client.get(
-            reverse('detail_user', args=[test_user.user_id]),
-            format='json'
-        )
-        get_user_resp = get_user_resp.json()
-
-        self.assertEqual(test_user.user_id, get_user_resp['user_id'])
-
-    def test_get_user_id_invalid(self):
-        get_user_resp = self.client.get(
-            reverse('detail_user', args=[1]),
-            format='json'
-        )
-
-        self.assertEqual(get_user_resp.status_code, status.HTTP_404_NOT_FOUND)

f381c505c629145ec550343a55433d24a18c07ea
Jude Adham <judeadham@gmail.com>
2022-03-01 14:26:53 -0500

    Button and name styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,15 +10,14 @@ function App () {
   return (
     <Container fluid>
       <Row>
-        <Col xs={4} lg={2} classname="align-middle" align='center' display='fluid'>
-          <h3>Name: Jess Adham</h3>
+        <Col xs={4} lg={2} className="d-flex align-items-end justify-content-center" >
+          <h4>Name: Jess Adham</h4>
         </Col>
         <Col xs={4} lg={8} align="center" className="mt-3">
           <h1>MakeMyDegree</h1>
         </Col>
-        <Col xs={4} lg={2}>
-          <p></p>
-          <Button variant="primary">Submit Plan</Button>{' '}
+        <Col xs={4} lg={2} className="d-flex align-items-end justify-content-center">
+          <Button className="w-75" variant="primary">Submit Plan</Button>
         </Col>
       </Row>
       <Row>

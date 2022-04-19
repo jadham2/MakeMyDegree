05db028861faad9ac1455f935901544bf5c3e72b
jess-jpg-txt <jesszhang24@gmail.com>
2022-04-19 14:25:38 -0400

    insert ece degree, ece course tags

--- backend/MakeMyDegree/scripts/loader.py
+++ backend/MakeMyDegree/scripts/loader.py
@@ -19,20 +19,82 @@ def all_purdue_courses_setup():
         if (course_resp.status_code == status.HTTP_400_BAD_REQUEST):
             print(a_course)
 
+def get_all_course_ids():
+    client = APIClient()
+    get_courses_resp = client.get(
+        reverse('create_get_courses'),
+        format='json'
+    )
+    get_courses_resp = get_courses_resp.json()
+    all_course_ids = dict()
+    for a_course in get_courses_resp:
+        all_course_ids[a_course["course_tag"]] = a_course["course_id"]
+    return all_course_ids
+
+# insert all Purdue course pre and co-requisites
+def all_purdue_requisites_setup():
+    return
+
 # insert a sample degree
-def ece_degree_setup(self):
-    return 
-    with open("../../degree_scrape/degrees/ece_tags.pickle", "rb") as f:
-        ece = json.load(f)
-    for a in ece:
-        pass
-    compe_degree_data = {
+def ece_degree_setup(all_course_ids):
+    # insert a ece degree
+    ece_degree = {
         'degree_type': 'BS',
         'degree_name': 'Computer Engineering',
-        'school': 'ECE',
+        'school': 'ENGR',
         'term': 'Fa2019'
     }
+    client = APIClient()
+    ece_resp = client.post(
+        reverse('create_get_degrees'),
+        data=ece_degree,
+        format='json'
+    )
+    assert (ece_resp.status_code == status.HTTP_201_CREATED)
+    ece_degree_id = ece_resp.json()["degree_id"]
+    
+    # insert all ece degree tags, along with the course tags
+    with open("MakeMyDegree/fixture/ece_tags.json", "r") as f:
+        ece = json.load(f)
+    for a_tag in ece:
+        a_ece_tag_data = {
+            'degree': ece_degree_id,
+            'name': a_tag["tag_name"],
+            'rule': '>='+str(a_tag["credits"])
+        }
+        client = APIClient()
+        tag_resp = client.post(
+            reverse('create_get_tags'),
+            data=a_ece_tag_data,
+            format='json'
+        )
+        if (tag_resp.status_code == status.HTTP_400_BAD_REQUEST):
+            print(a_ece_tag_data)
+        # update the course tags table
+        for a_course in a_tag["course_list"]:
+            try:
+                test_course_tag_data = {
+                    'course_id': all_course_ids[a_course],
+                    'tag_id': tag_resp.json()["tag_id"]
+                }
+                post_course_tag_resp = client.post(
+                    reverse('create_get_course_tags'),
+                    data=test_course_tag_data,
+                    format='json'
+                )
+                if (post_course_tag_resp.status_code == status.HTTP_400_BAD_REQUEST):
+                    print(test_course_tag_data)
+            except Exception as e: 
+                print(e)
+                
+# insert a test user
+def test_user_setup():
+    return
+
 
 def run():
     all_purdue_courses_setup()
-    ece_degree_setup()
+    all_course_ids = get_all_course_ids()
+    all_purdue_requisites_setup()
+    ece_degree_setup(all_course_ids)
+    test_user_setup()

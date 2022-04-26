4e68458d71ab4618e59cb7084353b311ef01e8af
jess-jpg-txt <jesszhang24@gmail.com>
2022-04-25 21:30:00 -0400

    test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'MakeMyDegree'
+    'MakeMyDegree',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [

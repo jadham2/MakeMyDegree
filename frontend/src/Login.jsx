f1b096fe3e606cf630782627415cc0ab4166634e
jess-jpg-txt <jesszhang24@gmail.com>
2022-05-02 18:36:09 -0400

    gradient background for log in page + app icon

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import {
   Row,
   Col,
-  Container,
   Button,
   Form
 } from 'react-bootstrap'
 import axios from 'axios'
+import logo from './app_logo.png'
+import { keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
 
 /* eslint-disable react/prop-types */
 
@@ -38,14 +40,38 @@ function Login (props) {
       }
     })
   }
+  const Gradient = keyframes`
+    0% {background-position: 0% 50%;}
+    50% {background-position: 100% 50%;}
+    100% {background-position: 0% 50%;}
+  `
+  const MyStyle = styled.div`
+    background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
+    background-size: 400% 400%;
+    animation: ${Gradient} 10s ease infinite;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `
+  const bounce = keyframes`
+    from, 20%, 53%, 80%, to {transform: translate3d(0,0,0);}
+    40%, 43% {transform: translate3d(0, -10px, 0);}
+    70% {transform: translate3d(0, -5px, 0);}
+    90% {transform: translate3d(0,-1px,0);}
+  `
+  const BounceStyle = styled.h1`animation: ${bounce} 2s ease infinite;`
 
   return (
-    <Container className="d-flex justify-content-center align-items-center vh-100">
+    <MyStyle>
       <Row>
+        <Col className="d-flex justify-content-center">
+          <img src={logo} alt="Logo" />
+        </Col>
         <Col>
           <Row>
             <Col className="d-flex justify-content-center">
-              <h1>MakeMyDegree</h1>
+              <BounceStyle>MakeMyDegree</BounceStyle>
             </Col>
           </Row>
           <Row>
@@ -86,7 +112,7 @@ function Login (props) {
           </Row>
         </Col>
       </Row>
-    </Container>
+    </MyStyle>
   )
 }
 

499a095cde3ec322018cc630ccf191e5a01b7a95
Jude Adham <judeadham@gmail.com>
2022-05-02 20:31:35 -0400

    Fixed rerender issues

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -11,6 +11,27 @@ import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 
 /* eslint-disable react/prop-types */
+const Gradient = keyframes`
+    0% {background-position: 0% 50%;}
+    50% {background-position: 100% 50%;}
+    100% {background-position: 0% 50%;}
+  `
+const MyStyle = styled.div`
+  background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
+  background-size: 400% 400%;
+  animation: ${Gradient} 10s ease infinite;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const bounce = keyframes`
+  from, 20%, 53%, 80%, to {transform: translate3d(0,0,0);}
+  40%, 43% {transform: translate3d(0, -10px, 0);}
+  70% {transform: translate3d(0, -5px, 0);}
+  90% {transform: translate3d(0,-1px,0);}
+`
+const BounceStyle = styled.h1`animation: ${bounce} 2s ease infinite;`
 
 function Login (props) {
   const [userName, setUserName] = useState()
@@ -40,27 +61,6 @@ function Login (props) {
       }
     })
   }
-  const Gradient = keyframes`
-    0% {background-position: 0% 50%;}
-    50% {background-position: 100% 50%;}
-    100% {background-position: 0% 50%;}
-  `
-  const MyStyle = styled.div`
-    background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
-    background-size: 400% 400%;
-    animation: ${Gradient} 10s ease infinite;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `
-  const bounce = keyframes`
-    from, 20%, 53%, 80%, to {transform: translate3d(0,0,0);}
-    40%, 43% {transform: translate3d(0, -10px, 0);}
-    70% {transform: translate3d(0, -5px, 0);}
-    90% {transform: translate3d(0,-1px,0);}
-  `
-  const BounceStyle = styled.h1`animation: ${bounce} 2s ease infinite;`
 
   return (
     <MyStyle>
